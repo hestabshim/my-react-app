c6f01562c6df87b8d7715799bc801c873747566b
Amber <amberzeng@gmail.com>
2025-02-08 22:35:20 -0500

    adding backend

--- backend/db.php
+++ backend/db.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "mydb.ics.purdue.edu";
-$username = "yourusername";
-$password = "your password";
-$dbname = "yourusername";
+$username = "zeng274";
+$password = "Wh0c@res?1234";
+$dbname = "zeng274";
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
 die("Connection failed: " . $conn->connect_error);
