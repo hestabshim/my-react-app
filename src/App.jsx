91567101eca0424fae588235b3ffad068cf4debf
Amber <amberzeng@gmail.com>
2025-02-16 23:15:27 -0500

    figuring things out

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,13 @@
-import './App.css'
-import About from './components/About'
-import NavBar from './components/Navbar'
-import alien from './assets/ouh.png'
-import amber from './assets/pfp1.jpeg'
-import Card from './components/maincard'
-import Wrapper from './components/wrapper'
+import "./App.css";
+import About from "./components/About";
+import Navbar from "./components/Navbar";
+import Card from "./components/maincard";
+import Wrapper from "./components/wrapper";
+import { useState } from "react";
 import ProfileForm from "./components/ProfileForm";
-import { useState } from 'react'
-import { useEffect } from 'react'
-import { use } from "react";
+import { useEffect } from "react";
 
 const App = () => {
-  const [profiles, setProfiles] = useState([]);
-  useEffect(() => {
-    fetch("https://web.ics.purdue.edu/~zeng274/profile-app/fetch-data.php")
-      .then((res) => res.json())
-      .then((data) => {
-        setProfiles(data);
-        console.log(data)
-      })
-  }, []);
-  
-  //Variable to store the animation state
-  const [animation, setAnimation] = useState(false);
-  //function to update the animation state
-  const handleAnimation = () => {
-    setAnimation(false);
-  };
 
   //Variable to store the mode state
   const [mode, setMode] = useState("light");
@@ -36,59 +17,69 @@ const App = () => {
   };
 
   // get titles
-  const titles = [...new Set(profiles.map((profile) => profile.title))];
+  const [titles, setTitles] = useState([]);
+  useEffect(() => {
+    fetch("https://web.ics.purdue.edu/~zeng274/profile-app/get-titles.php")
+    .then((res) => res.json())
+    .then((data) => {
+      setTitles(data.titles)
+    })
+  }, [])
 
   const [title, setTitle] = useState("");
-  //update the title on change of the downdrop
+  //update the title on change of the drowndrop
   const handleTitleChange = (event) => {
     setTitle(event.target.value);
-    setAnimation(true);
+    setPage(1);
   };
 
   const [search, setSearch] = useState("");
   //update the search on change of the input
   const handleSearchChange = (event) => {
     setSearch(event.target.value);
-    setAnimation(true);
+    setPage(1);
   };
+  const [profiles, setProfiles] = useState([]);
+  const [page, setPage] = useState(1);
+  const [count, setCount] = useState(1);
 
+  useEffect(() => {
+    fetch(`https://web.ics.purdue.edu/~zeng274/profile-app/fetch-data-with-filter.php?title=${title}&name=${search}&page=${page}&limit=10`)
+      .then((res) => res.json())
+      .then((data) => {
+        setProfiles(data.profiles);
+        setCount(data.count);
+        setPage(data.page);
+      })
+  }, [title,search, page]);
   //clear the title and search
   const handleClear = () => {
     setTitle("");
     setSearch("");
-    setAnimation(true);
+    setPage(1);
   };
 
-  //filter the profiles based on the title
-  const filterProfiles = profiles.filter(
-    (profile) =>
-      (title === "" || profile.title === title) &&
-      profile.name.toLowerCase().includes(search.toLowerCase())
-  );
   const buttonStyle = {
     border: "1px solid #ccc",
   };
 
-return (
+  return (
     <>
       <header>
-        
-      <NavBar mode={mode} updateMode={handleModeChange}/>
-        
-        
+        <Navbar mode={mode} updateMode={handleModeChange}/>
       </header>
       <main className={mode === "light" ? "light" : "dark"}>
-      <Wrapper>
-      <h1>Profile App </h1>
-      </Wrapper>
-      <Wrapper>
-        <About/>
-      </Wrapper>
-      <Wrapper>
+        <Wrapper>
+          <h1>Profile App</h1>
+        </Wrapper>
+        <Wrapper>
+          <About />
+        </Wrapper>
+        <Wrapper>
           <ProfileForm />
         </Wrapper>
-      <Wrapper>
-      <div className="filter-wrapper">
+        <Wrapper>
+          <div className="filter-wrapper">
             <div className="filter--select">
               <label htmlFor="title-select">Select a title:</label>
               <select
@@ -113,103 +104,40 @@ return (
                 value={search}
               />
             </div>
-            <button onClick={handleClear}>Clear</button>
+            <button onClick={handleClear} style={buttonStyle}>
+              <span className="sr-only">Reset</span>
+              
+            </button>
           </div>
           <div className="profile-cards">
-            {filterProfiles.map((profile) => (
+            {profiles.map((profile) => (
               <Card
-              key={profile.email}
-              {...profile}
-              animate={animation}
-              updateAnimate={handleAnimation}
-            />
+                key={profile.id}
+                {...profile}
+              />
             ))}
           </div>
+          {
+            count === 0 && <p>No profiles found!</p>
+          }
+          {count > 10 &&
+          <div className="pagination">
+            <button onClick={() => setPage(page - 1)} disabled={page === 1}>
+              <span className="sr-only">Previous</span>
+          
+              </button>
+            <span>{page}/{Math.ceil(count/10)}</span>
+            <button onClick={() => setPage(page + 1)} disabled={page >= Math.ceil(count/10)}>
+              <span className="sr-only">Next</span>
+              
+            </button>
+          </div>
+          }
+
         </Wrapper>
       </main>
     </>
   );
 };
 
-//   const profiles = [
-//     {
-//       id:0,
-//       img: alien,
-//       name: "Goobli Goo",
-//       title: "Alien",
-//       email: "goobligoo@gmail.com",
-//     },
-//     {
-//       id:1,
-//       img: amber,
-//       name: "Amber Zeng",
-//       title: "UX designer",
-//       email: "zeng274@purdue.edu",
-//     },
-//     {
-//       id:2,
-//       img: alien,
-//       name: "Bob Johnson",
-//       title: "Web developer",
-//       email: "c@a.com",
-//     },
-//     {
-//       id:3,
-//       img: alien,
-//       name: "Ava Smith",
-//       title: "Web developer",
-//       email: "d@a.com",
-//     },
-//     {
-//       id:4,
-//       img: alien,
-//       name: "Tom Smith",
-//       title: "Software Engineer",
-//       email: "e@a.com",
-//     },
-//     {
-//       id:5,
-//       img: alien,
-//       name: "Eva Smith",
-//       title: "Graphic designer",
-//       email: "f@a.com",
-//     }
-//   ]
-
-// ;
- 
-
-
-
-
-  /* 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-} */
-
-  export default App;
+export default App;
